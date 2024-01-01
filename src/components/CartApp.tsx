7686b563338a2fba9832409776a1bce23625123f
br8828982 <141433491+br8828982@users.noreply.github.com>
2024-01-01 19:19:42 +0530

    Update CartApp.tsx(Added heading)

--- src/components/CartApp.tsx
+++ src/components/CartApp.tsx
@@ -6,7 +6,9 @@ export function CartApp() {
   return (
     <>
       <h1>Shopping Cart</h1>
+      <h2>Store</h2>
       <StoreList />
+      <h2>Cart</h2>
       <CartList />
       <CartInfo />
     </>
